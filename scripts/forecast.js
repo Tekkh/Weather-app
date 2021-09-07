24ffb6b2cbfd1154263a5d8a135e3d0c9103c6ba
Tawfik <tkhorbi@gmail.com>
2021-09-07 14:27:07 +0100

    Making Class Forecast

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,3 +1,24 @@
+class Forecat{
+    constructor(){
+        this.key = 'TeLv0q740Nz3rdbY3Fhee8jkOVdhnOmo';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    }
+    async updateCity(city){
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+        return { cityDets, weather };
+    }
+    async getCity(city){
+       
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(cityURI + query)
+        const data = await response.json(); 
+        return data[0];
+
+    }
+}
+
 const key = 'TeLv0q740Nz3rdbY3Fhee8jkOVdhnOmo';
 
 // weather information
@@ -15,14 +36,7 @@ const getWeather = async (id) => {
 // city information
 const getCity = async (city) => {
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch(base + query)
-    const data = await response.json();
-
-    return data[0];
-
+    
 };
 
 getCity('oujda').then(data => {
